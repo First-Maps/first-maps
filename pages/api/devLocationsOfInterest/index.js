fddd70548c4a48232d0b8720e70608a28a281dcd
Kevin Hung <kevincjhung@gmail.com>
2022-10-20 20:13:23 -0700

    findMiddle works, filterLocations doesn't

--- pages/api/devLocationsOfInterest/index.js
+++ pages/api/devLocationsOfInterest/index.js
@@ -3,6 +3,9 @@
 import dbConnect from "../../../utils/dbConnect";
 import dev_LocationOfInterest from '../../../models/dev_LocationOfInterest';
 import axios from 'axios'
+// import testFunc from helper.js
+import findMiddleOfPolygon from '../../../utils/findMiddle'
+import filterPolygons from "../../../utils/filterLocations";
 
 dbConnect();
 
@@ -16,13 +19,20 @@ export default async (req, res) => {
                 // send axios request to nativelands api
                 const response = await axios.get('https://native-land.ca/api/index.php?maps=territories');
                 const locationsArray = response.data;
-                
+
+
+                // default parameters set in function d
+                // let filteredLocations = filterPolygons(locationsArray, undefined, undefined, undefined, undefined)
+
                 // loop through locationsArray, filter out the coordinates that are not in BC
+                // locationsArray.map((location) => { console.log(`polygon`, location.geometry.coordinates)})     
                 
-                const locationsOfInterest = await dev_LocationOfInterest.find({})
-                res.status(200).json({ success: true, data: locationsOfInterest })
+                
+
+                res.status(200).json({ success: true, data: filteredLocations })
+                // const locationsOfInterest = await dev_LocationOfInterest.find({})
             } catch(error){
-                res.status(400).json({ "error message" : error })
+                res.status(400).json({ "error message" : error.toString() })
             }
             break;
 
--- utils/findMiddle.js
+++ utils/findMiddle.js
@@ -1,17 +1,16 @@
-
 /**
- * @param {Array}  coordinates [longitude, latitude]
- * returns coordinate that is middle of the polygon,
- * Caltulated by using northernmost, southernmost, easternmost and westernmost points
+ * 
+ * @param {Array} polygon, accepts an array of arrays. Each array is a coordinate pair [longitude, latitude]
+ * @returns {Array} returns an array of the middle coordinate pair [longitude, latitude]
  */
-function findMiddle(arr) {
+function findMiddleOfPolygon(polygon) {
     let eastmost = [-180, 0];
     let westmost = [180, 0];
     let northmost = [0, -90];
     let southmost = [0, 90];
-    
+
     // find eastmost, westmost, northmost, southmost point in array, loop through all points
-    for( coordinate of arr){
+    for(let coordinate of polygon){
         if(coordinate[0] > eastmost[0]){
             eastmost = coordinate;
         }
@@ -29,8 +28,9 @@ function findMiddle(arr) {
     // find middle point between eastmost and westmost
     let middleLatitude = (eastmost[0] + westmost[0]) / 2;
     let middleLongitude = (northmost[1] + southmost[1]) / 2;
-    console.log("middleLatitude: ", middleLatitude);
     
-    return [middleLatitude, middleLongitude];
+    return [middleLongitude, middleLatitude];
 }
-export default findMiddle;
+
+
+export default findMiddleOfPolygon;
