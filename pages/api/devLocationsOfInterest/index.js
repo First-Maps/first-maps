d87e4527e7e7b6e812a3838a2024d3af61725154
Kevin Hung <kevincjhung@gmail.com>
2022-10-25 14:27:05 -0700

    Backend Data validation complete
    Note: it seems like either some of the info in the api response is missing or the formatting is different from the rest.
    So data validation is done in the route instead of making it required inside the schema definition.

--- pages/api/devLocationsOfInterest/index.js
+++ pages/api/devLocationsOfInterest/index.js
@@ -20,8 +20,39 @@ export default async (req, res) => {
 
         case 'POST':
             try {
-                const locationOfInterest = await dev_LocationOfInterest.create(req.body)
-                res.status(201).json({ success: true, data: locationOfInterest })
+
+                // validate the request body
+                if(req.body.name === undefined || req.body.name === null || req.body.name === "") {
+                    res.status(400).json({ "error message": "name is required" })
+                    break;
+                } else if (req.body.coordinates === undefined || req.body.coordinates === null || req.body.coordinates === "") {
+                    res.status(400).json({ "error message": "coordinates are required" })
+                    break;
+                } else if (req.body.coordinates.length !== 2) {
+                    res.status(400).json({ "error message": "coordinates must be an array of two numbers" })
+                    break;
+                } else if (typeof req.body.coordinates[0] !== "number" || typeof req.body.coordinates[1] !== "number") {
+                    res.status(400).json({ "error message": "coordinates must be an array of two numbers" })
+                    break;
+                } else if (req.body.description === undefined || req.body.description === null || req.body.description === "") {
+                    res.status(400).json({ "error message": "description is required" })
+                    break;
+                } else if (req.body.category === undefined || req.body.category === null || req.body.category === "") {
+                    res.status(400).json({ "error message": "category is required" })
+                    break;
+                }
+                
+                // formatting info from req.boy so that it can be passed into the mongoose function
+                let reqBodyObj = {
+                    name: req.body.name,
+                    description: req.body.description,
+                    category: req.body.category,
+                    coordinates: req.body.coordinates,
+                }
+
+                await dev_LocationOfInterest.create(reqBodyObj)
+
+                res.status(201).json({ success: true, data: reqBodyObj })
             } catch (error) {
                 res.status(400).json({ "error message": error.toString() })
             }
