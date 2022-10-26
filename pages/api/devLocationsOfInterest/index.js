51f455d8469a81b493ec94fabc4d63ff4ce2f24a
Yevgeniy Akimenko <yakimenko@bcit.ca>
2022-10-25 21:15:53 -0700

    cosmetics for locationsOfInterest files in api

--- pages/api/devLocationsOfInterest/index.js
+++ pages/api/devLocationsOfInterest/index.js
@@ -7,7 +7,7 @@ export default async (req, res) => {
   const { method } = req
 
   switch (method) {
-    case 'GET':
+    case "GET":
       try {
         const locationsOfInterest = await dev_LocationOfInterest.find({})
         res.status(200).json({ success: true, data: locationsOfInterest })
@@ -15,46 +15,72 @@ export default async (req, res) => {
         res.status(400).json({ "error message": error.toString() })
       }
       break
-        case 'POST':
-            try {
 
-                // validate the request body
-                if(req.body.name === undefined || req.body.name === null || req.body.name === "") {
-                    res.status(400).json({ "error message": "name is required" })
-                    break
-                } else if (req.body.coordinates === undefined || req.body.coordinates === null || req.body.coordinates === "") {
-                    res.status(400).json({ "error message": "coordinates are required" })
-                    break
-                } else if (req.body.coordinates.length !== 2) {
-                    res.status(400).json({ "error message": "coordinates must be an array of TWO numbers" })
-                    break
-                } else if (typeof req.body.coordinates[0] !== "number" || typeof req.body.coordinates[1] !== "number") {
-                    res.status(400).json({ "error message": "coordinates must be an array of two NUMBERS" })
-                    break
-                } else if (req.body.description === undefined || req.body.description === null || req.body.description === "") {
-                    res.status(400).json({ "error message": "description is required" })
-                    break
-                } else if (req.body.category === undefined || req.body.category === null || req.body.category === "") {
-                    res.status(400).json({ "error message": "category is required" })
-                    break
-                }
-                
-                // formatting info from req.boy so that it can be passed into the mongoose function
-                let reqBodyObj = {
-                    name: req.body.name,
-                    description: req.body.description,
-                    category: req.body.category,
-                    coordinates: req.body.coordinates,
-                }
+    case "POST":
+      try {
+        // validate the request body
+        if (
+          req.body.name === undefined 
+          || req.body.name === null 
+          || req.body.name === ""
+        ) {
+          res.status(400).json({ "error message": "name is required" })
+          break
+        } else if (
+          req.body.coordinates === undefined 
+          || req.body.coordinates === null 
+          || req.body.coordinates === ""
+        ) {
+          res.status(400).json({ "error message": "coordinates are required" })
+          break
+        } else if (req.body.coordinates.length !== 2) {
+          res.status(400).json({
+            "error message": "coordinates must be an array of TWO numbers",
+          })
+          break
+        } else if (
+          typeof req.body.coordinates[0] !== "number" 
+          || typeof req.body.coordinates[1] !== "number"
+        ) {
+          res.status(400).json({
+            "error message": "coordinates must be an array of two NUMBERS",
+          })
+          break
+        } else if (
+          req.body.description === undefined 
+          || req.body.description === null 
+          || req.body.description === ""
+        ) {
+          res.status(400).json({ "error message": "description is required" })
+          break
+        } else if (
+          req.body.category === undefined 
+          || req.body.category === null 
+          || req.body.category === ""
+        ) {
+          res.status(400).json({ "error message": "category is required" })
+          break
+        }
 
-                await dev_LocationOfInterest.create(reqBodyObj)
+        // formatting info from req.boy so that it can be passed into the mongoose function
+        let reqBodyObj = {
+          name: req.body.name,
+          description: req.body.description,
+          category: req.body.category,
+          coordinates: req.body.coordinates,
+        }
 
-                res.status(201).json({ success: true, data: reqBodyObj })
-            } catch (error) {
-                res.status(400).json({ "error message": error.toString() })
-            }
-            break
-        default:
-            res.status(400).json({ success: false, message: "This route does not exist" })
-    }
+        await dev_LocationOfInterest.create(reqBodyObj)
+
+        res.status(201).json({ success: true, data: reqBodyObj })
+      } catch (error) {
+        res.status(400).json({ "error message": error.toString() })
+      }
+      break
+
+      default:
+      res.status(400).json({
+        success: false, message: "This route does not exist" 
+      })
+  }
 }
--- pages/api/locationsOfInterest/index.js
+++ pages/api/locationsOfInterest/index.js
@@ -1,37 +1,37 @@
 // this will route to :     /api/locationsOfInterest
 
-import dbConnect from "../../../utils/dbConnect";
-import LocationOfInterest from "../../../models/LocationOfInterest";
+import dbConnect from "../../../utils/dbConnect"
+import LocationOfInterest from "../../../models/LocationOfInterest"
 
-import getLanguageData from "../../../utils/getLanguageData";
+import getLanguageData from "../../../utils/getLanguageData"
 
-dbConnect();
+dbConnect()
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default async (req, res) => {
-  const { method } = req;
+  const { method } = req
 
   switch (method) {
     case "GET":
       try {
         // find all locationsOfInterest
-        const locationsOfInterest = await LocationOfInterest.find({});
+        const locationsOfInterest = await LocationOfInterest.find({})
 
         // below is the code for lazy loading of language data if it doesn't exist in the database
         // but it won't be triggered because we have the same logic in the post route
-        let places = [];
+        let places = []
         for (let i = 0; i < locationsOfInterest.length; i++) {
           places.push({
             name: locationsOfInterest[i].name,
             description: locationsOfInterest[i].description,
             category: locationsOfInterest[i].category,
             coordinates: locationsOfInterest[i].coordinates,
-          });
+          })
 
           if (locationsOfInterest[i].languages.length === 0) {
-            console.log("languages absent, requesting from native-land.ca");
-            const [lng, lat] = locationsOfInterest[i].coordinates;
-            const languageData = await getLanguageData(lat, lng);
+            console.log("languages absent, requesting from native-land.ca")
+            const [lng, lat] = locationsOfInterest[i].coordinates
+            const languageData = await getLanguageData(lat, lng)
 
             places[i].languages =
               languageData.length === 0
@@ -41,49 +41,49 @@ export default async (req, res) => {
                       id: language.properties.ID, // just in case we need it later?
                       name: language.properties.Name.replace(/&#8217;/g, "'"), // replace html apostrophe symbols with real apostrophes
                       link: language.properties.description, // just in case we need it later?
-                    };
-                  });
+                    }
+                  })
 
             // then we do something like
             await LocationOfInterest.findByIdAndUpdate(
               locationsOfInterest[i]._id,
               places[i]
-            );
+            )
           }
         }
 
-        res.status(200).json({ success: true, results: locationsOfInterest });
+        res.status(200).json({ success: true, results: locationsOfInterest })
       } catch (error) {
-        res.status(400).json({ "error message": error.toString() });
+        res.status(400).json({ "error message": error.toString() })
       }
-      break;
+      break
 
     case "POST":
       try {
         // get the native language data
-        const [lng, lat] = req.body.coordinates;
-        const languageData = await getLanguageData(lat, lng);
+        const [lng, lat] = req.body.coordinates
+        const languageData = await getLanguageData(lat, lng)
 
-        const location = { ...req.body };
+        const location = { ...req.body }
         // add array of languages to location object
         location.languages = languageData.map((language) => {
           return {
             id: language.properties.ID, // just in case we need it later?
             name: language.properties.Name.replace(/&#8217;/g, "'"), // replace html apostrophe symbols with real apostrophes
             link: language.properties.description, // just in case we need it later?
-          };
-        });
+          }
+        })
 
-        const locationOfInterest = await LocationOfInterest.create(location);
-        res.status(201).json({ success: true, results: locationOfInterest });
+        const locationOfInterest = await LocationOfInterest.create(location)
+        res.status(201).json({ success: true, results: locationOfInterest })
       } catch (error) {
-        res.status(400).json({ "error message": error.toString() });
+        res.status(400).json({ "error message": error.toString() })
       }
-      break;
+      break
 
     default:
-      res
-        .status(400)
-        .json({ success: false, message: "This route does not exist" });
+      res.status(400).json({ 
+        success: false, message: "This route does not exist" 
+      })
   }
-};
+}
