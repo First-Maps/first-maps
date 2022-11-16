11e48ae32c0ed0a1ac4d56c637bc4fd8152f67fd
Yevgeniy Akimenko <yakimenko@bcit.ca>
2022-11-15 17:45:34 -0800

    * reshuffled the files in pages/explore for clarity * small changes in /explore/[category]/index for consistency

--- pages/explore/[category]/index.jsx
+++ pages/explore/[category]/index.jsx
@@ -1,51 +1,49 @@
 // Libraries
-import {useState, useEffect} from 'react'
+import { useState, useEffect } from 'react'
 import Head from 'next/head'
 import styled from "styled-components"
 import axios from 'axios'
 import { useRouter } from 'next/router'
 
 // Componenets
-import { Navbar } from '../../components/Navbar/Navbar'
-import ItemBox from '../../components/ItemBox/ItemBox'
+import { Navbar } from '../../../components/Navbar/Navbar'
+import ItemBox from '../../../components/ItemBox/ItemBox'
 
 
 
 export default function Explore({ ...props }){
-	const [categoryData, setcategoryData] = useState(false)
-	let router = useRouter()
-	let queryStr = useRouter().query.id // get the query string from the url
+  const [categoryData, setcategoryData] = useState(false)
+  let router = useRouter()
+  let queryStr = useRouter().query.category // get the query string from the url
 
   // styled components
-	// make the container centered column
-	const StyledItembox = styled.div`
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-		justify-content: center;
-	`
-	
-	
+  // make the container centered column
+  const StyledItembox = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+  `
 
 	useEffect(() => {
     const abortController = new AbortController()
 
-		// only fetch data after page is hydrated, so we have access to the 'queryStr' variable
-		if(!router.isReady){
-			return
-		}
-		
-		// if the query string is empty is not 'arts', 'history', 'language', or 'culture', redirect to the explore page
-		if (queryStr == undefined || (queryStr != 'arts' && queryStr != 'history' && queryStr != 'language' && queryStr != 'culture')) {
-			router.push('/explore')
-		}
-	
-		// fetch data for the category, after the query string is defined, then set the state
+    // only fetch data after page is hydrated, so we have access to the 'queryStr' variable
+    if(!router.isReady){
+      return
+    }
+
+    // if the query string is empty is not 'arts', 'history', 'language', or 'culture', redirect to the explore page
+    if (queryStr == undefined || (queryStr != 'arts' && queryStr != 'history' && queryStr != 'language' && queryStr != 'culture')) {
+      router.push('/explore')
+    }
+
+    // fetch data for the category, after the query string is defined, then set the state
     ;(async () => {
       try {
         let response = await axios.get(`/api/devLocationsOfInterest/${queryStr}`, { signal: abortController.signal })
-				let data = response.data.Results
-				setcategoryData(data)
+        let data = response.data.Results
+        setcategoryData(data)
       } catch (error) {
         console.error(error)
         if (axios.isCancel(error)) {
@@ -64,19 +62,19 @@ export default function Explore({ ...props }){
   	<div>
       <Head>
         <title>Explore</title>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/map-solid.svg" />
       </Head>
       <h1>{queryStr}</h1>
       
       <StyledItembox>
-				{ categoryData ? 
+        { categoryData ? 
           categoryData.map((categoryDataItem => {
             return <ItemBox 
               label={categoryDataItem.name}
               description={categoryDataItem.description}
               width="331.67px"
               height="230px"
-							key={categoryDataItem.id}
+              key={categoryDataItem._id}
             />
           }))
           : <p>Loading...</p>
