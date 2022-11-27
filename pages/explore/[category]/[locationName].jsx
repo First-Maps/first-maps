4d2745d204ec5e7bdecf2716242640fbe7988530
Kevin Hung <kevincjhung@gmail.com>
2022-11-26 18:13:58 -0800

    removed unnecessary comments

--- pages/explore/[category]/[locationName].jsx
+++ pages/explore/[category]/[locationName].jsx
@@ -34,11 +34,6 @@ export default function ExploreLocation() {
     }))
   }
 
-  // TODO: refer to code from /pages/explore/[category]/index.jsx
-  // use router.isReady to check if the router is ready
-  // get category & locationName only after it is done loading
-  // Bonus: skeleton load while it waits
-
 
 
   useEffect(() => {
